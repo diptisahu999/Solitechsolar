98699afe67a925205be1809c54205673a30bcdff
Diptiranjan <diptiranjansahu999@gmail.com>
2026-03-05 11:36:40 +0530

    add vCard access button issue

---
+++ fx_access_action_buttons/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Access Action Buttons in Gear Menu",
+    "summary": "Individual access for Export and Download (vCard)",
+    "version": "17.0.1.0",
+    "description": "Provide individual access checkboxes for Export and Download vCard actions in the gear menu.",
+    "author": "Antigravity",
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "depends": ["base", "web"],
+    "data": [
+        "security/action_security.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
